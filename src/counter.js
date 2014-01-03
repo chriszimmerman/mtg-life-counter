bf3ac6cc718b2ac7381ec8a4719f198d73679672
Chris Zimmerman <cjay.zimmerman@gmail.com>
2014-01-03 17:44:21 -0500

    MMF for one player

--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+    $('#player1up').click(function(){
+        var player1Life = Number($('#player1life').text());
+        player1Life++;
+        $('#player1life').text(player1Life);
+    });
 
+    $('#player1down').click(function(){
+        var player1Life = Number($('#player1life').text());
+        player1Life--;
+        $('#player1life').text(player1Life);
+    });
 });
