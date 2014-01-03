b2f94d59fdf23118a9e46111bf8ccd2943e39bb6
Chris Zimmerman <cjay.zimmerman@gmail.com>
2014-01-03 17:50:45 -0500

    MMF for 2 players

--- src/counter.js
+++ src/counter.js
@@ -10,4 +10,16 @@ $(document).ready(function(){
         player1Life--;
         $('#player1life').text(player1Life);
     });
+
+    $('#player2up').click(function(){
+        var player1Life = Number($('#player2life').text());
+        player1Life++;
+        $('#player2life').text(player1Life);
+    });
+
+    $('#player2down').click(function(){
+        var player1Life = Number($('#player2life').text());
+        player1Life--;
+        $('#player2life').text(player1Life);
+    });
 });
